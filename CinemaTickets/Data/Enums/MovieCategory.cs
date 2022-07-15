a3b6ea52923ce355885db1bf4cbc9da39a7d83a6
Andrej Shumkovski <85301264+Shume00@users.noreply.github.com>
2022-07-15 13:25:26 +0200

    -added models -initial database created

---
+++ CinemaTickets/Data/Enums/MovieCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace CinemaTickets.Data.Enums
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary,
+        Horror,
+        Animation
+    }
+}
---
+++ CinemaTickets/Models/Cinema.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CinemaTickets.Models
+{
+    public class Cinema
+    {
+        [Key]
+        public int Id { get; set; }
+        public string CinemaLogo { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Movie> Movies { get; set; }
+    }
+}
